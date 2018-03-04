6473ba8ea097268464f25458606a68bbedc9bdca
stiven <sduque866@gmail.com>
2018-03-04 17:44:07 -0500

    Se depura el codigo

--- app/src/main/java/com/stivenduque/myapplication/MainActivity.java
+++ app/src/main/java/com/stivenduque/myapplication/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 
 import android.widget.TextView;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
